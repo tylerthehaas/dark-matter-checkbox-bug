b9d0e3a53a29ab3de521127572091900b2f79d70
Tyler Haas <tyler-haas@pluralsight.com>
2021-01-06 12:14:30 -0700

    add a reproducing example of bug

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  Checkbox,
+  DarkMatterThemeProvider,
+  darkTheme,
+} from "@pluralsight/dark-matter-design-system";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DarkMatterThemeProvider theme={darkTheme}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell padding="checkbox">
+              <Checkbox />
+            </TableCell>
+            <TableCell>Channel Group</TableCell>
+            <TableCell>Created</TableCell>
+            <TableCell>Last Updated</TableCell>
+          </TableRow>
+        </TableHead>
+      </Table>
+    </DarkMatterThemeProvider>
   );
 }
 
